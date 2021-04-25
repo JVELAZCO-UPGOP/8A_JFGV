ddf3f1b24d8604ecc70b263a21b5eba5233ed737
FernandoUwU <79486772+FernandoUwU@users.noreply.github.com>
2021-04-25 18:54:24 -0500

    Delete veterinaria-fullstack directory

--- veterinaria-fullstack/backend/index.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const requestHandler = require("./request-handler");
-const server = http.createServer(requestHandler);
-
-server.listen(5000, () => {
-  console.log("El servidor esta escuchando peticiones en http://localhost:5000/");
-});
